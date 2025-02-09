c87c8593ef54209f77527fbe86e068154e09ce5e
leolangley <155990606+leolangley@users.noreply.github.com>
2025-02-09 17:20:40 -0500

    2/7 DN

--- DataAnalyzer.java
+++ DataAnalyzer.java
@@ -1,11 +1,14 @@
 import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
 public class DataAnalyzer {
-    
+
     public static int linearSearch(int[] arr, int number){
         for (int i = 0; i < arr.length; i++){
             if (arr[i] == number){
-                return i;
+                return i+1;
             }
         }
         return -1;
@@ -42,20 +45,49 @@ public class DataAnalyzer {
     }
 
     public static void main(String[] args) {
-        int[] data = {1, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10};
-        int number = 7;
+        // int[] data = {1, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10};
+        // int number = 7;
         
-        int linearOutcome = linearSearch(data, number);
+        // int linearOutcome = linearSearch(data, number);
+        // System.out.println("Linear Search Result: " + linearOutcome);
+
+        // Arrays.sort(data);
+        // int binaryOutcome = binarySearch(data, number);
+        // System.out.println("Binary Search Outcome: " + binaryOutcome);
+
+        // reverseList(data);
+        // System.out.println("Reversed List: ");
+        // for (int i = 0; i < data.length; i++){
+        //     System.out.print(i + " ");
+        // }
+
+        int[] arr = new int[100];
+
+        try{
+            File f = new File("numbers.txt");
+            Scanner input = new Scanner(f);
+
+            for (int i = 0; i < 100; i++){
+                arr[i] = input.nextInt();
+            }
+        }
+
+        catch (IOException e){
+            System.out.println("File not found!");
+        }
+
+        int number = 7;
+        int linearOutcome = linearSearch(arr, number);
         System.out.println("Linear Search Result: " + linearOutcome);
 
-        Arrays.sort(data);
-        int binaryOutcome = binarySearch(data, number);
+        Arrays.sort(arr);
+        int binaryOutcome = binarySearch(arr, number);
         System.out.println("Binary Search Outcome: " + binaryOutcome);
 
-        reverseList(data);
+        reverseList(arr);
         System.out.println("Reversed List: ");
-        for (int i = 0; i < data.length; i++){
-            System.out.print(i + " ");
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
         }
     }
 
